4ace573a87e98919208ddb6d76987972aedf65df
Daisuke2525 <148730269+Daisuke2525@users.noreply.github.com>
2024-01-16 10:25:05 +0900

    あ

---
+++ src/db-connect.php
@@ -0,0 +1,7 @@
+<?php
+const SERVER = 'mysql220.phy.lolipop.lan';
+const DBNAME = 'LAA1517363-final';
+const USER = 'LAA1517363';
+const PASS = 'Pass0303';
+$connect = 'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8';
+?>
